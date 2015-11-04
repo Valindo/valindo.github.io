9eb717210f1da7d5dff4856da626b4da0597e089
valindo godinho <valindo.godinho@gmail.com>
2015-11-04 16:54:41 +0200

    Added test form

---
+++ formtest/assets/js/app.js
@@ -0,0 +1,26 @@
+(function(){
+	var app = angular.module('testing',[]);
+
+	app.controller('test', ['$scope','$http', function($scope, $http){
+		$scope.check = function(){
+			console.log($scope.name);
+			console.log($scope.emails);
+			var request = $http({
+				method:"post",
+				url:"https://plugzee-signup.herokuapp.com/new",
+				params: {
+					name: $scope.name,
+					email: $scope.emails,
+				}
+			});
+
+			request.success(
+                    function( html ) {
+                        $scope.cfdump = html;
+                    }
+                );
+			// console.log($scope.cfdump);
+		};
+	}]);
+
+})();

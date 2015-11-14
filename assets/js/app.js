ff1c5e6d8b84116634cb69cd3c2adb291d0bb03f
valindo godinho <valindo.godinho@gmail.com>
2015-11-14 16:44:59 +0200

    Semi final build

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,21 @@
 (function(){
 	var app = angular.module('landingPage',['ngDialog']);
-
 	app.config(function($httpProvider) {
       //Enable cross domain calls
       $httpProvider.defaults.useXDomain = true;
-
       //Remove the header used to identify ajax call  that would prevent CORS from working
       delete $httpProvider.defaults.headers.common['X-Requested-With'];
   });
-
 	app.controller('MainCtrl',function($scope, ngDialog){
 		$scope.clickToOpen = function (){
 			console.log("I reached here");
 			ngDialog.open({ template: 'test.html'});
 		};
 	});
-
 	app.controller('VideoCtrl',function($scope){
 		$scope.w = $(window).width();
 		$scope.w = String($scope.w);
 	});
-
 	app.controller('MenuCtrl',function($scope){
 		$scope.show = true;
 		$scope.showMe = function(){
@@ -28,43 +23,35 @@
 			console.log($scope.show);
 		};
 	});
-
 	app.controller('FromSubmit', ['$scope','$http', function($scope, $http, transformRequestAsFormPost){
-// 	$scope.check = function(){
-	// 		console.log($scope.name);
-	// 		console.log($scope.emails);
-	// 		var request = $http({
-	// 			method:"post",
-	// 			url:"http://127.0.0.1:4567/new",
-	// 			transformRequest: transformRequestAsFormPost,
-	// 			data: {
-	// 				name: $scope.name,
-	// 				email: $scope.emails,
-	// 			}
-	// 		});
-	// 		console.log(typeof request);
-	// 		// console.log(request.toSource());
-	// 		// console.log($scope.cfdump);
-	// 	};
-	$scope.post = function(names,emails) {
-		if ($scope.signupForm.$valid){
-			$http.post("http://plugzee-signup.herokuapp.com/new", { name: names, email: emails }).success(function(result) {
-				console.log(result);
-				$scope.resultPost = result;
-				console.log("*********")
-				$scope.result = true;
-			}).error(function() {
-				console.log("error");
-			});
-		} else {
-			console.log("Error");
+		$scope.post = function(names,emails) {
+			if ($scope.signupForm.$valid){
+				$http.post("http://plugzee-signup.herokuapp.com/new", { name: names, email: emails }).success(function(result) {
+					console.log(result);
+					$scope.resultPost = result;
+					console.log("*********")
+					$scope.result = true;
+				}).error(function() {
+					console.log("error");
+				});
+			} else {
+				console.log("Error");
+			}
+		};
+		$scope.again = function(){
+			$scope.result = false;
+		};
+	}]);
+
+	app.directive('ngAnimation',function(){
+		return {
+			restrict: 'A',
+			require: '^ngModel',
+			scope:{
+				ngModel: '=',
+				ngWidth: '@',
+			},
+			template:'<div id="player-target" anm-player-target anm-src="http://clips.animatron.com/9d2aa5da1421219c96c9684a3835846a.json" anm-importer="animatron" anm-width="{{ngWidth-10}}" anm-height="400" anm-rib-color="#81d5f2" anm-bg-color="#81d5f2" anm-auto-play="true" anm-controls="false" ></div>'
 		}
-	};
-
-	$scope.again = function(){
-		$scope.result = false;
-	};
-
-}]);
-
+	});
 })();

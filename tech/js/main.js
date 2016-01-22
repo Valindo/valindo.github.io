a0f44040b170f828c552bc10d4bc28601bd9af04
valindo godinho <valindo.godinho@gmail.com>
2016-01-23 01:38:16 +0530

    Clearing up

--- tech/js/main.js
+++
@@ -1,8 +0,0 @@
-var mainslideHeight = $(window).height() - $('.menu').height();
-$('.mainslide').css('height', mainslideHeight);
-
-function testing(){
-	$('#mech').removeClass('hidden');
-	$('#mech').addClass('section');
-	$('#events').addClass('hidden');
-};

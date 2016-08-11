0a0efaf5cd25553711507da20aad7e2fc15fe2f0
Valindo Godinho <valindo.godinho@gmail.com>
2016-08-11 23:26:30 +0530

    fodela re

--- gulpfile.js
+++ gulpfile.js
@@ -241,7 +241,7 @@ gulp.task('clean', function (cb) {
 
 gulp.task('serveprod', function() {
   connect.server({
-    root: [process.env.ROOT],
+    root: [$ROOT],
     port: process.env.PORT || 5000, // localhost:5000
     livereload: false
   });
